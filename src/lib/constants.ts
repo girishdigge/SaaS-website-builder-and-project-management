57dff194681a52e88d78486c477e022a04acc97d
Girish Digge <girishdigge@gmail.com>
2024-02-02 19:42:58 +0530

    prisma Schema created

---
+++ src/lib/constants.ts
@@ -0,0 +1,20 @@
+export const pricingCards = [
+  {
+    title: 'Starter',
+    description: 'Perfect for trying out plura',
+    price: 'Free',
+    duration: '',
+    highlight: 'Key features',
+    features: ['3 Sub accounts', '2 Team members', 'Unlimited pipelines'],
+    priceId: '',
+  },
+  {
+    title: 'Unlimited Saas',
+    description: 'The ultimate agency kit',
+    price: '$199',
+    duration: 'month',
+    highlight: 'Key features',
+    features: ['Rebilling', '24/7 Support team'],
+    priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+  },
+];
